e072dc49200ffe73a910a753f8aa0b2f142f0c74
Alexander Bolinsky <alexander.bolinsky@woven-planet.global>
2023-12-07 17:07:20 +0900

    Add test

--- 01_intermediate_functions_and_data_structures/02_challenge/test.py
+++ 01_intermediate_functions_and_data_structures/02_challenge/test.py
@@ -4,20 +4,22 @@ from apply_discount import apply_discount
 class TestApplyDiscount(unittest.TestCase):
     def test_apply_discount_1(self):
         self.assertEqual(
-            extract_data("User details: Name:John. He is very active. He is also quite tall, standing at Height:181cm. He is Age:30, and spends his time as an Occupation:Engineer. End of data."),
-            [("Name", "John"), ("Height", "181cm"), ("Age", "30"), ("Occupation", "Engineer")]
+            apply_discount([
+                {"name": "Laptop", "category": "Electronics", "price": 1200},
+                {"name": "Bread", "category": "Food", "price": 2},
+                {"name": "Jacket", "category": "Apparel", "price": 100}
+            ], 50, 10),
+            [("Laptop", 1080.0), ("Jacket", 90.0)]
         )
 
     def test_apply_discount_2(self):
         self.assertEqual(
-            extract_data("Product ID:12345, in the Category:Electronics department, priced at Price:299. All rights reserved."),
-            [("ID", "12345"), ("Category", "Electronics"), ("Price", "299")]
-        )
-
-    def test_apply_discount_3(self):
-        self.assertEqual(
-            extract_data("Emergency Contacts: Police:911, Fire:999, Medical:112. Stay safe."),
-            [("Police", "911"), ("Fire", "999"), ("Medical", "112")]
+            apply_discount([
+                {"name": "Smartphone", "category": "Electronics", "price": 800},
+                {"name": "Sneakers", "category": "Footwear", "price": 120},
+                {"name": "Coffee", "category": "Beverage", "price": 5}
+            ], 100, 15),
+            [("Smartphone", 680.0), ("Sneakers", 102.0)]
         )
 
 if __name__ == '__main__':
