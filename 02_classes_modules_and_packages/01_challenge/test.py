bfe417c7d4000c60fe87785b0871cc74b2f1bca1
Yuma Sumi <yuma.sumi@woven-planet.global>
2024-03-17 02:23:53 +0900

    refactored test 02-01

--- 02_classes_modules_and_packages/01_challenge/test.py
+++ 02_classes_modules_and_packages/01_challenge/test.py
@@ -3,45 +3,45 @@ from library import Library
 
 
 class TestLibrary(unittest.TestCase):
-    data = [
-        {"title": "1984", "author": "George Orwell"},
-        {"title": "To Kill a Mockingbird", "author": "Harper Lee"},
-    ]
-
     def setUp(self) -> None:
         self.library = Library()
         return super().setUp()
 
     def test_add_book(self):
-        book = self.data[0]
+        data = {"title": "1984", "author": "George Orwell"}
 
         self.library.add_book(
-            title=book["title"],
-            author=book["author"],
+            title=data["title"],
+            author=data["author"],
         )
 
         books = self.library.retrieve_books()
 
-        self.assertEqual(books[0]["title"], book["title"])
-        self.assertEqual(books[0]["author"], book["author"])
+        self.assertEqual(len(books), 1)
+        self.assertEqual(books[0]["title"], data["title"])
+        self.assertEqual(books[0]["author"], data["author"])
 
     def test_remove_book(self):
         """Note: No error handling is expected."""
-        book = self.data[0]
+
+        data = {"title": "1984", "author": "George Orwell"}
 
         self.library.add_book(
-            title=book["title"],
-            author=book["author"],
+            title=data["title"],
+            author=data["author"],
         )
 
-        self.library.remove_book(book["title"])
+        self.library.remove_book(data["title"])
 
         self.assertEqual(len(self.library.retrieve_books()), 0)
 
     def test_retrieve_books(self):
-        self.assertEqual(len(self.library.retrieve_books()), 0)
+        data = [
+            {"title": "1984", "author": "George Orwell"},
+            {"title": "To Kill a Mockingbird", "author": "Harper Lee"},
+        ]
 
-        for book in self.data:
+        for book in data:
             self.library.add_book(
                 title=book["title"],
                 author=book["author"],
@@ -49,11 +49,14 @@ class TestLibrary(unittest.TestCase):
 
         books = self.library.retrieve_books()
 
-        self.assertEqual(len(books), len(self.data))
+        self.assertEqual(len(books), len(data))
 
         for i, book in enumerate(books):
-            self.assertEqual(book["title"], self.data[i]["title"])
-            self.assertEqual(book["author"], self.data[i]["author"])
+            self.assertEqual(book["title"], data[i]["title"])
+            self.assertEqual(book["author"], data[i]["author"])
+
+    def test_retrieve_books_empty(self):
+        self.assertEqual(len(self.library.retrieve_books()), 0)
 
     def tearDown(self) -> None:
         self.library = None
