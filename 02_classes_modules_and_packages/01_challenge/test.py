f83ebcc27c1ec003653a704dc4a234aaad6b55c3
Yuma Sumi <yuma.sumi@woven-planet.global>
2024-03-16 23:07:31 +0900

    fixed test for 02-01

--- 02_classes_modules_and_packages/01_challenge/test.py
+++ 02_classes_modules_and_packages/01_challenge/test.py
@@ -1,33 +1,64 @@
 import unittest
 from library import Library
 
+
 class TestLibrary(unittest.TestCase):
-    def test_add_and_remove_book(self):
-        lib = Library()
-        lib.add_book("1984", "George Orwell")
-        lib.add_book("To Kill a Mockingbird", "Harper Lee")
-        lib.remove_book("1984")
+    data = [
+        {"title": "1984", "author": "George Orwell"},
+        {"title": "To Kill a Mockingbird", "author": "Harper Lee"},
+    ]
 
-        has_book = False
-        for book in lib.retrieve_books():
-            if "To Kill a Mockingbird" in book:
-                has_book = True
-        self.assertTrue(has_book)
+    def setUp(self) -> None:
+        self.library = Library()
+        return super().setUp()
 
-        has_book = False
-        for book in lib.retrieve_books():
-            if "1984" in book:
-                has_book = True
-        self.assertFalse(has_book)
+    def test_add_book(self):
+        book = self.data[0]
+
+        self.library.add_book(
+            title=book["title"],
+            author=book["author"],
+        )
+
+        books = self.library.retrieve_books()
+
+        self.assertEqual(books[0]["title"], book["title"])
+        self.assertEqual(books[0]["author"], book["author"])
+
+    def test_remove_book(self):
+        """Note: No error handling is expected."""
+        book = self.data[0]
+
+        self.library.add_book(
+            title=book["title"],
+            author=book["author"],
+        )
+
+        self.library.remove_book(book["title"])
+
+        self.assertEqual(len(self.library.retrieve_books()), 0)
 
     def test_retrieve_books(self):
-        lib = Library()
-        lib.add_book("The Great Gatsby", "F. Scott Fitzgerald")
-        has_book = False
-        for book in lib.retrieve_books():
-            if "The Great Gatsby" in book:
-                has_book = True
-        self.assertTrue(has_book)
+        self.assertEqual(len(self.library.retrieve_books()), 0)
 
-if __name__ == '__main__':
+        for book in self.data:
+            self.library.add_book(
+                title=book["title"],
+                author=book["author"],
+            )
+
+        books = self.library.retrieve_books()
+
+        self.assertEqual(len(books), len(self.data))
+
+        for i, book in enumerate(books):
+            self.assertEqual(book["title"], self.data[i]["title"])
+            self.assertEqual(book["author"], self.data[i]["author"])
+
+    def tearDown(self) -> None:
+        self.library = None
+        return super().tearDown()
+
+
+if __name__ == "__main__":
     unittest.main()
